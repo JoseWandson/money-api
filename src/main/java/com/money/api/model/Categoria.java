a6fed3fd82180cf8d63d9f3bdc2021f885fe4cba
José Wandson Cavalcante Siqueira <wandsonacop@hotmail.com>
2019-11-18 21:32:30 -0300

    Criação do listar categorias

---
+++ src/main/java/com/money/api/model/Categoria.java
@@ -0,0 +1,26 @@
+package com.money.api.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Entity
+@EqualsAndHashCode
+@Table(name = "categoria")
+public class Categoria {
+
+	@Id
+	@EqualsAndHashCode.Include
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long codigo;
+	private String nome;
+
+}
