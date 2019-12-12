d8f1839c6a810df7689d92c28d204282e762edd3
José Wandson Cavalcante Siqueira <wandsonacop@hotmail.com>
2019-12-11 21:00:55 -0300

    Validando lançamento com Bean Validation.

--- src/main/java/com/money/api/dto/LancamentoDTO.java
+++ src/main/java/com/money/api/dto/LancamentoDTO.java
@@ -3,6 +3,9 @@ package com.money.api.dto;
 import java.math.BigDecimal;
 import java.time.LocalDate;
 
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+
 import com.money.api.model.Categoria;
 import com.money.api.model.Pessoa;
 import com.money.api.model.TipoLancamento;
@@ -14,11 +17,22 @@ import lombok.Setter;
 @Setter
 public class LancamentoDTO {
 
+	@NotBlank
 	private String descricao;
+
+	@NotNull
 	private LocalDate dataVencimento;
+
+	@NotNull
 	private BigDecimal valor;
+
+	@NotNull
 	private TipoLancamento tipo;
+
+	@NotNull
 	private Categoria categoria;
+
+	@NotNull
 	private Pessoa pessoa;
 
 }
