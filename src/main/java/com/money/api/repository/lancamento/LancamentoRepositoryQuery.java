35d7873874ef36f19a48bb955ed24b8fc00e70fe
José Wandson Cavalcante Siqueira <wandsonacop@hotmail.com>
2019-12-14 14:19:16 -0300

    Implementando a paginação de lançamentos.

--- src/main/java/com/money/api/repository/lancamento/LancamentoRepositoryQuery.java
+++ src/main/java/com/money/api/repository/lancamento/LancamentoRepositoryQuery.java
@@ -1,12 +1,13 @@
 package com.money.api.repository.lancamento;
 
-import java.util.List;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 
 import com.money.api.model.Lancamento;
 import com.money.api.repository.filter.LancamentoFilter;
 
 public interface LancamentoRepositoryQuery {
 
-	public List<Lancamento> filtrar(LancamentoFilter lancamentoFilter);
+	Page<Lancamento> filtrar(LancamentoFilter lancamentoFilter, Pageable pageable);
 
 }
--- src/main/java/com/money/api/service/LancamentoService.java
+++ src/main/java/com/money/api/service/LancamentoService.java
@@ -1,9 +1,10 @@
 package com.money.api.service;
 
-import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
 import com.money.api.model.Lancamento;
@@ -22,8 +23,8 @@ public class LancamentoService {
 	@Autowired
 	private PessoaRepository pessoaRepository;
 
-	public List<Lancamento> pesquisar(LancamentoFilter lancamentoFilter) {
-		return lancamentoRepository.filtrar(lancamentoFilter);
+	public Page<Lancamento> pesquisar(LancamentoFilter lancamentoFilter, Pageable pageable) {
+		return lancamentoRepository.filtrar(lancamentoFilter, pageable);
 	}
 
 	public Optional<Lancamento> buscarPeloCodigo(Long codigo) {
