b724e17f604b990d4deb5a8ec4539edc1301bebc
José Wandson Cavalcante Siqueira <wandsonacop@hotmail.com>
2021-02-04 20:54:00 -0300

    Criando a entidade Contato para suportar mestre-detalhe

---
+++ src/main/java/com/money/api/model/Contato.java
@@ -0,0 +1,45 @@
+package com.money.api.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Entity
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
+@Table(name = "contato")
+public class Contato {
+
+	@Id
+	@EqualsAndHashCode.Include
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long codigo;
+
+	@NotBlank
+	private String nome;
+
+	@Email
+	@NotBlank
+	private String email;
+
+	@NotBlank
+	private String telefone;
+
+	@NotNull
+	@ManyToOne
+	@JoinColumn(name = "codigo_pessoa")
+	private Pessoa pessoa;
+
+}
--- src/main/java/com/money/api/model/Pessoa.java
+++ src/main/java/com/money/api/model/Pessoa.java
@@ -1,12 +1,17 @@
 package com.money.api.model;
 
+import java.util.List;
+
+import javax.persistence.CascadeType;
 import javax.persistence.Embedded;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.OneToMany;
 import javax.persistence.Table;
 import javax.persistence.Transient;
+import javax.validation.Valid;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
 
@@ -29,6 +34,10 @@ public class Pessoa {
 	private Endereco endereco;
 
 	private Boolean ativo;
+
+	@Valid
+	@OneToMany(mappedBy = "pessoa", cascade = CascadeType.ALL)
+	private List<Contato> contatos;
 
 	@Transient
 	@JsonIgnore
